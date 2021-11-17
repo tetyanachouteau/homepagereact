706140fac20d554878ab07e923fcf37a41c4d5ca
tetyana <tetyana.chouteau@gmail.com>
2021-11-17 20:47:17 +0100

    htmlcssreact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+        <h1>Le bon service</h1>
+        <p>Echange de Services entre particuliers</p>
+        <form>
+          <input type="search" name="q" placeholder="Rechercher" />
+          <input type="submit" value="Lancer" />
+        </form>
       </header>
-    </div>
+
+      <main>
+        <aside>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Loisir</a>
+              </li>
+              <li>
+                <a href="#">Sport</a>
+              </li>
+              <li>
+                <a href="#">Covoiturage</a>
+              </li>
+              <li>
+                <a href="#">Autre</a>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <article>
+          <div class="card">
+            <div class="favori">
+              <button>3</button>
+            </div>
+            <a href="#" class="picture">
+              <img src="anglais.jpeg" alt="" />
+            </a>
+            <h2>
+              <a href="#">Cours d'anglais</a>
+            </h2>
+            <p class="price">Pour les débutants</p>
+            <p class="location">Paris 75009</p>
+          </div>
+          <div class="card">
+            <div class="favori">
+              <button>3</button>
+            </div>
+            <a href="#" class="picture">
+              <img src="enfant.png" alt="" />
+            </a>
+            <h2>
+              <a href="#">Baby sitting</a>
+            </h2>
+            <p class="price">WE et mercredi</p>
+            <p class="location">Paris 75012</p>
+          </div>
+          <div class="card">
+            <div class="favori">
+              <button>3</button>
+            </div>
+            <a href="#" class="picture">
+              <img src="fee.gif" alt="" />
+            </a>
+            <h2>
+              <a href="#">Menage</a>
+            </h2>
+            <p class="price">Lundi et mardi</p>
+            <p class="location">Argenteuil 95100</p>
+          </div>
+        </article>
+      </main>
+      <footer>
+        <p>tati</p>
+      </footer>
+    </body>
   );
 }
 
