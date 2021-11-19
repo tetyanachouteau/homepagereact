4be46e096ea35236d478a650f53308cea08b9e12
tetyana <tetyana.chouteau@gmail.com>
2021-11-19 13:47:28 +0100

    Css à tester

--- src/App.js
+++ src/App.js
@@ -7,37 +7,45 @@ function App() {
   return (
     <body>
       <header>
-      <div class="line">
-        <h1>Le bon service</h1>
-        <p>Echange de Services entre particuliers</p>
-        <h1 class="logo"><img src="textencouleur.JPG" alt="" /></h1> 
-        <form>
-          <input type="search" name="q" placeholder="Rechercher" />
-          <input type="submit" value="Lancer" />
-        </form>
+        <div class="line">
+          <h1>Le bon service</h1>
+          <p>Echange de Services entre particuliers</p>
+          <h1 class="logo">
+            <img src="textencouleur.JPG" alt="" />
+          </h1>
+          <form>
+            <input type="search" name="q" placeholder="Rechercher" />
+            <input type="submit" value="Lancer" />
+          </form>
         </div>
-      
-          <nav>
-            <ul>
-              <li>
-                <a href="#">Loisir</a>
-              </li>
-              <li>
-                <a href="./index.html">Sport</a>
-              </li>
-              <li>
-                <a href="#">Covoiturage</a>
-              </li>
-              <li>
-                <a href="#">Autre</a>
-              </li>
-            </ul>
-          </nav>
-          </header>
-          <main>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Loisir</a>
+            </li>
+            <li>
+              <a href="./index.html">Sport</a>
+            </li>
+            <li>
+              <a href="#">Covoiturage</a>
+            </li>
+            <li>
+              <a href="#">Autre</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
         <aside>
+          <input ref="" type="text" />
+          <button>Details</button>
+        </aside>
         <article>
           <Card></Card>
+          <h1>titre de l'article</h1>
+          <p></p>
+          <p></p>
         </article>
       </main>
       <footer>
